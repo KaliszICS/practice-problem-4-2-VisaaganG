ca483076130d060f20612da509f776b129be8aa8
VisaaganG <343505582@tdsb.ca>
2026-04-13 15:42:53 +0000

    Questions Finished

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,43 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
-	}
-
+	public static String repeatLetter(char letter, int times) {
+    String result = "";
+    for (int i = 0; i != times; i++) {
+        result += letter;
+    }
+    return result;
+}
+	public static int countLetter(String text, char letter) {
+    int count = 0;
+    
+    for (int i = 0; i != text.length(); i++) {
+        if (text.charAt(i) == letter) {
+            count++;
+        }
+    }
+    
+    return count;
+}
+	public static int evenSum(int a, int b) {
+    int start;
+	int end;
+    int sum = 0;
 	
+    if (a < b) {
+        start = a;
+        end = b;
+    } else {
+        start = b;
+        end = a;
+    }
 
+    for (int i = start; i <= end; i++) {
+        
+        if (i % 2 == 0) {
+            sum += i;
+        }
+    }
+
+    return sum;
+}
 }
